bce2e7f2cbff6e52c0698750a246bd54f9d4e399
Viacheslav Kolybelkin <Viacheslav_Kolybelkin@epam.com>
2021-07-31 23:20:00 +0400

    Initial commit

---
+++ src/main/kotlin/app/kolybelkin/kuberapp1/KuberApp1Application.kt
@@ -0,0 +1,11 @@
+package app.kolybelkin.kuberapp1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KuberApp1Application
+
+fun main(args: Array<String>) {
+	runApplication<KuberApp1Application>(*args)
+}
---
+++ src/test/kotlin/app/kolybelkin/kuberapp1/KuberApp1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.kolybelkin.kuberapp1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KuberApp1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
